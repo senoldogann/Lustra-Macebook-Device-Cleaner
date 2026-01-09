a8841d2fe259b6040a43e56e2d767e2d44eff12b
senoldogann <s.dogann@yandex.com>
2026-01-09 20:16:44 +0200

    Initial Commit

---
+++ MacCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacCleaner
+//
+//  Created by dogan on 9.1.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
